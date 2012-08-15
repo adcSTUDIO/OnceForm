625e8a0ea354a564bcfec3e330873ea4c9358d96
Kevin Newman <CaptainN@unFocus.com>
2012-08-15 17:29:18 -0400

    Commit the basic set

---
+++ OnceForm.php
@@ -0,0 +1,285 @@
+<?php
+include 'validtors.php';
+
+/*
+The OnceForm - Write once HTML5 forms processing for PHP.
+
+Copyright (C) 2012  adcSTUDIO LLC
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/**
+ * The OnceForm Write your form once, wrap it in a function.
+ * Pass that function name to a new OnceForm and it wires up the
+ * request validation!
+ *
+ * @author Kevin Newman <Kevin@adcSTUDIO.com>
+ * @package The OnceForm
+ * @copyright (C) 2012 adcSTUDIO LLC
+ * @license GNU/GPL, see license.txt
+ */
+class OnceForm
+{
+	public $isRequest = false;
+	public $isValid = false;
+	public $validators = array();
+	public $data = array();
+	
+	public function __toString() {
+		$form = $this->form;
+		return $form->toString();
+	}
+	
+	/**
+	 * Creates a OnceForm.
+	 * 
+	 * @param function $form_func A function set up to output (echo) an 
+	 * HTML5 to the user. This function's output will be captured to an
+	 * output buffer. Note: If a form func is passed to the constructor,
+	 * the OnceForm will automatically check and validate the request.
+	 */
+	public function __construct( $form_func = NULL )
+	{
+		if ( !is_null( $form_func ) )
+		{
+			$this->add_form_func( $form_func );
+			
+			$this->check_request();
+			
+			if ( $this->isRequest )
+				$this->isValid = $this->validate( $data );
+		}
+	}
+	
+	/**
+	 * Add a form function to the OnceForm.
+	 * 
+	 * @param function $func A function setup to output (echo) an 
+	 * HTML5 to the user. This function's output will be captured to an
+	 * output buffer.
+	 */
+	public function add_form_func( $func )
+	{
+		ob_start();
+		call_user_func( $func );
+		
+		$this->parse_form( ob_get_clean() );
+	}
+	
+	/**
+	 * Set the form to process from an HTML string.
+	 *
+	 * @param string $html The html5 form to be automatically processed.
+	 */
+	public function parse_form( $html )
+	{
+		// Padding with div to workaround a limitation in ganon, where root element
+		// attributes are not accessible.
+		$this->parser = new HTML_Parser_HTML5( '<div>' . $html . '</div>' );
+		
+		$root = $this->parser->root;
+		$forms = $root('form');
+		
+		$form = $this->form = $forms[0];
+		
+		// fixes HTML5 self closing issue with ganon.
+		foreach( $form('*') as $elem ) {
+			$elem->self_close_str="";
+		}
+	}
+	
+	/**
+	 * Checks the request object, to see if a request has been made, and sets
+	 * the form elements' value props to the submitted data. If a form func
+	 * was passed to the constructor, this will be automatically called on
+	 * construction. Otherwise, it must be called manually.
+	 */
+	public function check_request()
+	{
+		$form = $this->form;
+		
+		if ( 'POST' == strtoupper( $form->method ) )
+			$data = $_POST;
+		else
+			// the default `method` if none specified is GET
+			$data = $_GET;
+		
+		// If the $data array is empty, nothing was sent to the server,
+		// so we aren't doing a postback.
+		if ( empty( $data ) )
+			$this->data = $this->get_default_data();
+		
+		else {
+			$this->isRequest = true;
+			
+			// This checks the form values have return values, and polyfills if not.
+			$this->data = $data = $this->set_request_data( $data );
+		}
+	}
+	
+	/**
+	 * Builds (polyfills) the $data property. Useful when you want to get
+	 * the default data when there has been no reqeust.
+	 */
+	protected function get_default_data()
+	{
+		$form = $this->form;
+		
+		$inputs = $form('input');
+		
+		$data = array();
+		
+		foreach ( $inputs as $input ) {
+			switch( $input->type ) {
+				default:
+					$data[ $input->name ] = $input->value;
+			}
+		}
+		
+		return $data;
+	}
+	
+	/**
+	 * Normalizes the request data (polyfills anything missing) and 
+	 * sets the form value props to the request data.
+	 * @param the request dat to filter.
+	 */
+	public function set_request_data( $data )
+	{
+		$form = $this->form;
+		
+		// :TODO: expand for other form types
+		$inputs = $form("input");
+		
+		// Every field might be skipped for various reasons
+		// so we'll normalize the request data.
+		foreach( $inputs as $input )
+		{
+			switch( $input->type )
+			{
+				// These are a bit special, they may need the checked prop added
+				case 'radio':
+				case 'checkbox':
+					if ( isset( $data[ $input->name ] ) )
+					{
+						// if the request data contains the element name
+						// and is a checkbox, then it's checked
+						if ( $input->type == 'checkbox' ) {
+							$input->addAttribute( 'checked', 'checked' );
+						}
+						// radios are only marked "checked" if the value matches
+						else if (
+							$input->type == 'radio' &&
+							$input->value == $data[ $input->name ]
+						)
+							$input->addAttribute( 'checked', 'checked' );
+					}
+					else {
+						// poly fill to prevent php errors
+						$data[ $input->name ] = '';
+						
+						// the box is unchecked
+						$input->deleteAttribute( 'checked' );
+					}
+					
+					// :TODO: Figure out if more polyfilling is required for sets
+					
+				break;
+				
+				// The may be blank if the disabled flag is set
+				case 'email':
+				case 'text':
+				case 'hidden':
+				default:
+					// set value prop to request value
+					if ( isset( $data[ $input->name ] ) )
+						$input->value = $data[ $input->name ];
+					// or set request value to elem.value prop
+					else if ( isset( $input->value ) )
+						$data[ $input->name ] = $input->value;
+					// or default to empty
+					else
+						$data[ $input->name ] = '';
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * Validates the OnceForm against the data passed.
+	 *
+	 * @param array $data The request data (or other) to validate 
+	 * against the OnceForm.
+	 * @return boolean Whether or not the data is valid by the 
+	 * rules of the OnceForm.
+	 */
+	public function validate( $data )
+	{
+		$form = $this->form;
+		
+		if (
+			$this->validate_inputs( $form('input'), $data ) &&
+			$this->validate_selects( $form('select'), $data ) &&
+			$this->validate_textareas( $form('textarea'), $data )
+		)
+			return true;
+		else
+			return false;
+	}
+	
+	private function validate_inputs( $inputs, $data )
+	{
+		$valid = true;
+		
+		foreach( $inputs as $input )
+		{
+			switch( $input->type )
+			{
+				case 'email':
+					$validator = new EmailValidator( $input );
+				break;
+				
+				case 'radio':
+				case 'checkbox':
+				case 'text':
+				case 'hidden':
+				default:
+					$validator = new InputValidator( $input );
+				break;
+			}
+			
+			$this->validators[] = $validator;
+			
+			if ( !$validator->validate() )
+				$valid = false;
+		}
+		
+		return $valid;
+	}
+	
+	private function validate_selects( $selects, $data )
+	{
+		return true;
+	}
+	
+	private function validate_textareas( $textareas, $data )
+	{
+		return true;
+	}
+	
+	private $parser;
+	private $form;
+}
---
+++ validtors.php
@@ -0,0 +1,241 @@
+<?php
+/*
+The OnceForm - Write once HTML5 forms processing for PHP.
+
+Copyright (C) 2012  adcSTUDIO LLC
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/**
+ * InputValidatr - validates a basic input type="text" and serves as
+ * the base for other validators.
+ * 
+ * @author Kevin Newman <Kevin@adcSTUDIO.com>
+ * @package The OnceForm
+ * @copyright (C) 2012 adcSTUDIO LLC
+ * @license GNU/GPL, see license.txt
+ */
+class InputValidator
+{
+	public $name;
+	public $value;
+	public $required;
+	
+	public $isValid;
+	
+	public $errors = array();
+	
+	/**
+	 * Validates Input fields. Validates text fields, and is
+	 * a base for other input type validators.
+	 * @param stdObect|array The properties to validate.
+	 */
+	public function __construct( $props = NULL )
+	{
+		if ( is_array( $props ) )
+			$props = (object) $props;
+		
+		if ( !is_null( $props ) )
+		{
+			if ( isset( $props->name ) )
+				$this->name = $props->name;
+			
+			if ( isset( $props->value ) )
+				$this->value = $props->value;
+			
+			if ( isset( $props->required ) )
+				$this->required = $props->required;
+		}
+	}
+	
+	public function validate()
+	{
+		if ( $this->required && empty( $this->value ) )
+			$this->errors[] = "required field is empty";
+		
+		return $this->isValid = empty( $this->errors );
+	}
+}
+
+class NumbericValidator extends InputValidator
+{
+	public $step;
+	public $max;
+	public $min;
+	
+	public function __construct( $props = NULL )
+	{
+		parent::__construct( $props );
+		
+		if ( is_array( $props ) )
+			$props = (object) $props;
+		
+		if ( !is_null( $props ) )
+		{
+			if ( isset( $props->min ) )
+				$this->min = $props->min;
+			
+			if ( isset( $props->max ) )
+				$this->max = $props->max;
+			
+			if ( isset( $props->step ) )
+				$this->step = $props->step;
+		}
+	}
+	
+	public function validate()
+	{
+		parent::validate();
+		
+		switch ( $node->type )
+		{
+			case 'number':
+			case 'range':
+				if ( !is_numeric( $node->value ) )
+					$this->errors[] = 'not a number';
+				
+				if ( !empty( $this->step ) &&
+						!self::step( $node->value, $this->step ) )
+					$this->errors[] = 'not a valid step of ' . $this->step;
+				
+				if ( !empty( $this->max ) &&
+						!self::max( $node->value, $this->max ) )
+					$this->errors[] = 'not below maximum of ' . $this->max;
+				
+				if ( !emtpy( $this->min ) &&
+						!self::min( $node->value, $this->min ) )
+					$this->errors[] = 'not above minimum of '. $this->min;
+				
+				break;
+			case 'date':
+			case 'datetime':
+			case 'datetime-local':
+			case 'month':
+			case 'time':
+			case 'week':
+				break;
+		}
+		
+		return $this->isValid = empty( $this->errors );
+	}
+	
+	static public function step( $value, $step )
+	{
+		return $value % $step !== 0;
+	}
+	
+	static public function max( $value, $max )
+	{
+		return $value <= $max;
+	}
+	
+	static public function min( $value, $min )
+	{
+		return $value >= $min;
+	}
+}
+
+class PatternValidator extends InputValidator
+{
+	public $pattern;
+	
+	public function __construct( $props = NULL )
+	{
+		parent::__construct( $props );
+		
+		if ( is_array( $props ) )
+			$props = (object) $props;
+		
+		if ( !is_null( $props ) )
+		{
+			if ( isset( $props->pattern ) )
+				$this->pattern = $props->pattern;
+		}
+	}
+	
+	public function validate()
+	{
+		parent::validate();
+		
+		if ( !empty($this->pattern) &&
+				!preg_match( $This->$pattern, $this->value ) )
+			$this->errors[] = 'not a valid value';
+		
+		return $this->isValid = empty( $this->errors );
+	}
+}
+
+/**
+ * Validates email fields.
+ */
+ 
+class EmailValidator extends PatternValidator
+{
+	static private $email_pattern = "/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/";
+	
+	public function __construct( $props = NULL )
+	{
+		parent::__construct( $props );
+	}
+	
+	/**
+	 * Validates under two conditions:
+	 * <ul>
+	 * <li>If the field is required, always validates.</li>
+	 * <li>If the field is not required, but is filled in.</li>
+	 * </ul>
+	 * @return boolean Whether or not the field validates.
+	 */
+	public function validate()
+	{
+		parent::validate();
+		
+		if ( $this->required || strlen( $this->value ) > 0 ) { 
+			if ( !preg_match( self::$email_pattern, $this->value ) )
+				$this->errors[] = 'not a valid email address';
+		}
+		
+		return $this->isValid = empty( $this->errors );
+	}
+}
+
+class SelectValidator extends InputValidator
+{
+	private $options;
+	
+	// $props->value should contain the value of the selected option
+	public function __construct( $props = NULL, $options = array() )
+	{
+		parent::__construct( $props );
+		
+		if ( is_array( $props ) )
+			$props = (object) $props;
+		
+		if ( !empty( $options ) )
+			$this->options = $options;		
+	}
+	
+	public function validate()
+	{
+		parent::validate();
+		
+		if ( true )
+		{
+			
+		}
+	}
+	
+}
